46f0b73ffc1434c57ff5ae9ba17d926b8345cb7a
TravisTuRing <travisturing403@gmail.com>
2020-01-17 11:28:57 +0800

    back up

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -10,13 +10,13 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=25" };
-static const char dmenufont[]       = "monospace:size=25";
+static const char *fonts[]          = { "Lucida Mac:size=25" };
+static const char dmenufont[]       = "Lucida Mac:size=25";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#37474f";
+static const char col_cyan[]        = "#0099CC";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -42,6 +42,12 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "netease-cloud-music",       NULL,       NULL,       4 << 9,       1,           -1 },
+	{ "ss-qt5",       NULL,       NULL,       4 << 9,       1,           -1 },
+	{ "Connection Manager",       NULL,       NULL,       4 << 9,       1,           -1 },
+	{ "qq",       NULL,       NULL,       4 << 9,       1,           -1 },
+	{ "QQ",       NULL,       NULL,       4 << 9,       1,           -1 },
+	{ "electronic-wechat",       NULL,       NULL,       4 << 9,       1,           -1 },
 };
 
 /* layout(s) */
@@ -57,13 +63,14 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY    Mod1Mask
-#define SUPER     Mod4Mask
+#define MODKEY    Mod4Mask
+// #define SUPER     Mod1Mask  // Mod1Mask as alt
 #define VOLMUTE   0x1008ff12
 #define VOLDOWM   0x1008ff11
 #define VOLUP     0x1008ff13
 #define LIGHTDOWN 0x1008ff03
 #define LIGHTUP   0x1008ff02
+#define LOCK      0xffc6
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -79,28 +86,30 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 static const char *trayercmd[]  = { "/usr/ring/scripts/tr-toggle.sh", NULL };
+static const char *lockon[]  = { "xscreensaver-command --lock", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 // control 
 static const char *upvol[] = {"pamixer", "-i",  "10", NULL};
 static const char *dowmvol[] = {"pamixer", "-d",  "10", NULL};
 static const char *mutevol[] = {"pamixer", "-t", NULL};
-static const char *lightup[] = {"light", "-A", "10", NULL};
-static const char *lightdowm[] = {"light", "-U", "10", NULL};
+static const char *lightup[] = {"light", "-A", "5", NULL};
+static const char *lightdowm[] = {"light", "-U", "5", NULL};
 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ SUPER,                        XK_space,  spawn,          {.v = dmenucmd } },
-	{ SUPER,                        XK_Return, spawn,          {.v = termcmd } },
-	{ SUPER,                        XK_c,      spawn,          {.v = browsercmd } },
-	{ SUPER,                        XK_t,      spawn,          {.v = trayercmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = trayercmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ 0,                            VOLMUTE,   spawn,          {.v = mutevol       } },
 	{ 0,                            VOLDOWM,   spawn,          {.v = dowmvol       } },
 	{ 0,                            VOLUP,     spawn,          {.v = upvol         } },
 	{ 0,                            LIGHTDOWN ,spawn,          {.v = lightdowm     } },
 	{ 0,                            LIGHTUP,   spawn,          {.v = lightup       } },
+	{ 0,                            LOCK,      spawn,          {.v = lockon       } },
     // hide bar
 	//{ MODKEY,                       XK_b,      togglebar,      {0} },
     // reverse window
@@ -121,8 +130,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	//{ MODKEY,                       XK_space,  setlayout,      {0} },
+	//{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
