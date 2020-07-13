23c6d985633525ee04ce65a7123bfca1c23ce60f
66RING <1552087953@qq.com>
2020-07-13 21:36:09 +0800

    update

--- config.h
+++ config.h
@@ -156,20 +156,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-
-    // size control
-    { MODKEY,					    XK_Down,	moveresize,		{.v = (int []){ 0, 25, 0, 0 }}},
-    { MODKEY,					    XK_Up,		moveresize,		{.v = (int []){ 0, -25, 0, 0 }}},
-    { MODKEY,					    XK_Right,	moveresize,		{.v = (int []){ 25, 0, 0, 0 }}},
-    { MODKEY,					    XK_Left,	moveresize,		{.v = (int []){ -25, 0, 0, 0 }}},
-    { MODKEY|ShiftMask,			    XK_Down,	moveresize,		{.v = (int []){ 0, 0, 0, 25 }}},
-    { MODKEY|ShiftMask,			    XK_Up,		moveresize,		{.v = (int []){ 0, 0, 0, -25 }}},
-    { MODKEY|ShiftMask,			    XK_Right,	moveresize,		{.v = (int []){ 0, 0, 25, 0 }}},
-    { MODKEY|ShiftMask,			    XK_Left,	moveresize,		{.v = (int []){ 0, 0, -25, 0 }}},
-    { MODKEY|ShiftMask|ControlMask, XK_Down,	moveresize,		{.v = (int []){ 0, 0, 0, 75 }}},
-    { MODKEY|ShiftMask|ControlMask, XK_Up,		moveresize,		{.v = (int []){ 0, 0, 0, -75 }}},
-    { MODKEY|ShiftMask|ControlMask, XK_Right,	moveresize,		{.v = (int []){ 0, 0, 75, 0 }}},
-    { MODKEY|ShiftMask|ControlMask, XK_Left,	moveresize,		{.v = (int []){ 0, 0, -75, 0 }}},
 };
 
 /* button definitions */
